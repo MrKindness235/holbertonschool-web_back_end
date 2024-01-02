924083630e4e070168c2b10182f7009ff286c258
MrKindness235 <patriciocoitino235@gmail.com>
2024-01-02 05:04:31 -0800

    Another task is done.

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""type-annotated function floor which takes a float n as argument and returns the floor of the float."""
+
+
+def floor(n: float) -> int:
+    """Returns floor of float."""
+    return int(n)
