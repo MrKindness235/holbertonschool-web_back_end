f92c4d059cb6b0b3f9d0e554d88d77feacd3bb58
MrKindness235 <patriciocoitino235@gmail.com>
2024-01-02 04:52:48 -0800

    Some requierements were amiss: /python3 -> /env python3; all files must be executable.

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """type-anotated function 'add' that takes a float a and a float b as
  arguments and returns their sum as a float"""
 
 
 def add(a: float, b: float) -> float:
-    """Returns a + b"""
+    """Returns sum of float and float b"""
     return a + b
