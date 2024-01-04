b7927f2a2a04efb2ab40d5c604489392db8a8eda
MrKindness235 <patriciocoitino235@gmail.com>
2024-01-04 08:23:46 -0800

    Thanks Aaron.

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -6,5 +6,8 @@ argument and returns a function that multiplies a float by multiplier."""
 from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable:
-    return 
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Such a novel concept! A function inside a function!"""
+    def carlos(x: float) -> float:
+        return x * multiplier
+    return carlos(multiplier)
