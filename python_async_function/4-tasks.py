b4e713cfe0713e58680ddd008f87e28cfd1be516
MrKindness235 <patriciocoitino235@gmail.com>
2024-01-14 11:13:37 -0800

    Done.

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,8 +7,8 @@ task_wait_random = __import__('4-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Function that repeats wait_random (n) times."""
-    lista = []
-    for _ in range(0, n):
-        lista.append(await task_wait_random(max_delay))
-    return sorted(lista)
+    """ returns a list of tasks """
+    float_list: List[float] = []
+    for i in range(n):
+        float_list.append(await task_wait_random(max_delay))
+    return sorted(float_list)
