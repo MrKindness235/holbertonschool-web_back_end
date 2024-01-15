7249e80b0c1fbc0ac8543703f6f3b1712f684cdd
MrKindness235 <patriciocoitino235@gmail.com>
2024-01-15 04:54:36 -0800

    finally...

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,28 +1,28 @@
 export default class Currency {
-    constructor(code, name) {
-      this._code = code;
-      this._name = name;
-    }
-  
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
     // Getter
-  
-    get code() {
-      return this._code;
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    set setCode(newCode) {
-      this._code = newCode;
-    }
-  
-    set setName(newName) {
-      this._name = newName;
-    }
-  
-    displayFullCurrency() {
-      return `${this._name} (${this._code})`;
-    }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set setCode(newCode) {
+    this._code = newCode;
+  }
+
+  set setName(newName) {
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -2,35 +2,35 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    if (typeof sqft !== 'number') throw TypeError('Sqft must be a number');
-    if (typeof floors !== 'number') throw TypeError('Floors must be a number');
-    super(sqft);
-    this._floors = floors;
+  if (typeof sqft !== 'number') throw TypeError('Sqft must be a number');
+  if (typeof floors !== 'number') throw TypeError('Floors must be a number');
+  super(sqft);
+  this._floors = floors;
   }
 
   // Getters
 
   get sqft() {
-    return this._sqft;
+  return this._sqft;
   }
 
   get floors() {
-    return this._floors;
+  return this._floors;
   }
 
   // Setters
 
   set sqft(newSqft) {
-    if (typeof newSqft !== 'number') throw TypeError('Sqft must be a number');
-    this._sqft = newSqft;
+  if (typeof newSqft !== 'number') throw TypeError('Sqft must be a number');
+  this._sqft = newSqft;
   }
 
   setFloors(newfloor) {
-    if (typeof newfloor !== 'number') throw TypeError('Floors must be a number');
-    this._floors = newfloor;
+  if (typeof newfloor !== 'number') throw TypeError('Floors must be a number');
+  this._floors = newfloor;
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors`;
+  return `Evacuate slowly the ${this.floors} floors`;
   }
 }
