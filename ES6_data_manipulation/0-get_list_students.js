1b5a7ae72d56e0d2b0ff141e19dad2d17fa7d7a6
MrKindness235 <patriciocoitino235@gmail.com>
2024-01-31 11:53:55 -0800

    Identation.

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,19 @@
 export default function getListStudents() {
   const Students = [
     {
-        firstName: 'Guillaume',
-        id: 1,
-        location: 'San Francisco',
+      firstName: 'Guillaume',
+      id: 1,
+      location: 'San Francisco',
     },
     {
-        firstName: 'James',
-        id: 2,
-        location: 'Columbia',
+      firstName: 'James',
+      id: 2,
+      location: 'Columbia',
     },
     {
-        firstName: 'Serena',
-        id: 5,
-        location: 'San Francisco',
+      firstName: 'Serena',
+      id: 5,
+      location: 'San Francisco',
     },
   ];
 
